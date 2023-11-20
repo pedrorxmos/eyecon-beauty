55eb743f6c851e20667a26e83eae21ffa5a6da9e
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-11-20 22:41:40 +0100

    💄 EB-39 Header nav basic structure

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,9 +1,43 @@
 import React from 'react'
 
 import './Header.scss'
+import Link from 'next/link'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+      <div className="header-container">
+        <div className="header--logo">
+          EYECON BEAUTY
+        </div>
+
+        <nav className="header--nav">
+          <ul className="header--nav-list">
+            <li className="header--nav-item">
+              <Link href="#" className="header--nav-link">Shop</Link>
+              <ul className='header--nav-sublist'>
+                <li className="header--nav-subitem">
+                  <Link href="#" className="header--nav-sublink">Eyeshadows</Link>
+                  <ul className="header--nav-sublist">
+                  <li className="header--nav-subitem">
+                    <Link href="#" className="header--nav-sublink">Matte Eyeshadows</Link>
+                  </li>
+                  <li className="header--nav-subitem">
+                    <Link href="#" className="header--nav-sublink">Shimmer Eyeshadows</Link>
+                  </li>
+                  </ul>
+                </li>
+                <li className="header--nav-subitem">
+                  <Link href="#" className="header--nav-sublink">Palettes</Link>
+                </li>
+                <li className="header--nav-subitem">
+                  <Link href="#" className="header--nav-sublink">All Products</Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   )
 }
