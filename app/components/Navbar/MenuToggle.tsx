becf2f205a1da1e7b6f3320236a5a22eca485f45
Pedro Ramos <pedrorxmosdev@gmail.com>
2024-01-02 20:01:39 +0100

    💄 EB-39 Close mobile menu when click on link

--- app/components/Navbar/MenuToggle.tsx
+++ app/components/Navbar/MenuToggle.tsx
@@ -1,49 +1,48 @@
-"use client"
-import React, { MouseEventHandler, useEffect } from 'react'
-import { Icon } from '..'
+'use client';
+import React, { MouseEventHandler, useEffect } from 'react';
+import { Icon } from '..';
+import { usePathname } from 'next/navigation';
 
 export const MenuToggle = () => {
- 
+	const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+		const body = document.querySelector('body');
+		const navbarMenuMobile = document.querySelector('.navbar-menu-mobile');
+		const target = e.target as HTMLButtonElement;
+		const animations = target.querySelectorAll('polyline animate') as NodeListOf<SVGAnimateElement>;
+		navbarMenuMobile?.classList.toggle('open');
+		target.classList.toggle('open');
 
-  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const body = document.querySelector('body')
-    const navbarMenuMobile = document.querySelector('.navbar-menu-mobile')
-    const target = e.target as HTMLButtonElement
-    const animations = target.querySelectorAll('polyline animate') as NodeListOf<SVGAnimateElement>
-    navbarMenuMobile?.classList.toggle('open')
-    target.classList.toggle('open')
+		animations.forEach((animation: SVGAnimateElement) => {
+			if (target.classList.contains('open')) {
+				if (animation.id.includes('open')) {
+					animation.beginElement();
+				}
+			} else {
+				if (animation.id.includes('close')) {
+					animation.beginElement();
+				}
+			}
+		});
 
-    animations.forEach((animation: SVGAnimateElement) => {
-      if(target.classList.contains('open')) {
-        if(animation.id.includes('open')) {
-          animation.beginElement();
-        }
-      } else {
-        if(animation.id.includes('close')) {
-          animation.beginElement();
-        }
-      }
-    })
-    
-    body?.classList.toggle('overflow-hidden')
-  }
- 
-  useEffect(() => {
-    const body = document.querySelector('body')
-    const navbar = document.querySelector('.navbar')
-    if(navbar) {
-      const assignNavbarHeight = () => {
-        body?.style.setProperty('--navbar-height', navbar?.clientHeight + 'px')
-      }
-      assignNavbarHeight();
-    
-      window.addEventListener('resize', assignNavbarHeight)
-    }
-  }, [])
+		body?.classList.toggle('overflow-hidden');
+	};
 
-  return (
-    <button className="no-style navbar-main-item" onClick={onClick}>
-      <Icon name="navmenu" className='no-events' size='medium' weight='regular'/>
-    </button>
-  )
-}
+	useEffect(() => {
+		const body = document.querySelector('body');
+		const navbar = document.querySelector('.navbar');
+		if (navbar) {
+			const assignNavbarHeight = () => {
+				body?.style.setProperty('--navbar-height', navbar?.clientHeight + 'px');
+			};
+			assignNavbarHeight();
+
+			window.addEventListener('resize', assignNavbarHeight);
+		}
+	}, []);
+
+	return (
+		<button className="no-style navbar-main-item" onClick={onClick}>
+			<Icon name="navmenu" className="no-events" size="medium" weight="regular" />
+		</button>
+	);
+};
--- app/components/Navbar/mobileButton.tsx
+++ app/components/Navbar/mobileButton.tsx
@@ -1,39 +1,57 @@
-"use client"
-import React, { useEffect } from 'react'
+'use client';
+import React, { useEffect } from 'react';
 import { Links } from '../../interfaces/';
 import { Icon } from '..';
 
 interface MobileButtonProps {
-  link: Links
+	link: Links;
 }
 
-export const MobileButton = ({link}: MobileButtonProps) => {
-  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    let target = e.target as HTMLButtonElement;
-    
-    if(target.nodeName != 'BUTTON')
-      target = target.parentElement as HTMLButtonElement
+export const MobileButton = ({ link }: MobileButtonProps) => {
+	const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+		let target = e.target as HTMLButtonElement;
+		if (target.nodeName != 'BUTTON') target = target.parentElement as HTMLButtonElement;
+		target.parentElement?.classList.toggle('open');
+	};
 
-    target.parentElement?.classList.toggle('open')
-  }
+	useEffect(() => {
+		const submenus = document.querySelectorAll('.submenu, .subsubmenu');
+		submenus.forEach((m) => {
+			const menu = m as HTMLElement;
+			let scrollHeight = menu.scrollHeight;
+			if (menu.querySelector('.subsubmenu')) {
+				scrollHeight += menu.querySelector('.subsubmenu')!.scrollHeight;
+			}
 
-  useEffect(() => {
-    const submenus = document.querySelectorAll('.submenu, .subsubmenu');
-    submenus.forEach((m) => {
-      const menu = m as HTMLElement
-      let scrollHeight = menu.scrollHeight
-      if(menu.querySelector('.subsubmenu')) {
-        scrollHeight += menu.querySelector('.subsubmenu')!.scrollHeight
-      }
+			menu.style.setProperty('--max-height', scrollHeight + 'px');
+			menu.style.setProperty('--mhs', scrollHeight + '');
+		});
 
-      menu.style.setProperty('--max-height', scrollHeight + 'px')
-      menu.style.setProperty('--mhs', scrollHeight + '')
-    })
-  }, [])
-  return (
-    <button className="menu-link" onClick={onClick}>
-      <span>{link.title['en']}</span>
-      <Icon name="chevron-down" size="medium" />
-    </button>
-  )
-}
+		const navbarMenuMobile = document.querySelector('.navbar-menu-mobile');
+		const toggle = document.querySelector('.navbar-toggle button');
+		const animations = toggle?.querySelectorAll('polyline animate') as NodeListOf<SVGAnimateElement>;
+		const body = document.querySelector('body');
+
+		document
+			.querySelector('.navbar')
+			?.querySelectorAll('a')
+			.forEach((a) => {
+				a.addEventListener('click', () => {
+					navbarMenuMobile?.classList.remove('open');
+					toggle?.classList.remove('open');
+					body?.classList.remove('overflow-hidden');
+					animations.forEach((animation: SVGAnimateElement) => {
+						if (animation.id.includes('close')) {
+							animation.beginElement();
+						}
+					});
+				});
+			});
+	}, [link]);
+	return (
+		<button className="menu-link" onClick={onClick}>
+			<span>{link.title['en']}</span>
+			<Icon name="chevron-down" size="medium" />
+		</button>
+	);
+};
